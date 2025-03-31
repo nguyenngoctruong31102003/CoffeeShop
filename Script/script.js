2035d1bb131764efd1174fdc3332dccfe14cebe3
nguyenngoctruong31102003 <nguyenngoctruong31102003@gmail.com>
2025-04-01 01:21:32 +0700

    finsh

--- Script/script.js
+++ Script/script.js
@@ -22,56 +22,55 @@ window.addEventListener("load", () => {
 });
 
 // Start login Facebook Account
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "587983370944235",
-    cookie: true,
-    xfbml: true,
-    version: "v18.0",
-  });
-console.log(FB.version)
-  FB.AppEvents.logPageView();
-};
-  (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "587983370944235",
+//     cookie: true,
+//     xfbml: true,
+//     version: "v18.0",
+//   });
+//   FB.AppEvents.logPageView();
+// };
+//   (function (d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = "https://connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
 
 // Log In
-function loginWithFacebook() {
-  FB.login(
-    function (response) {
-      if (response.authResponse) {
-        // Người dùng đã đăng nhập, lấy thông tin người dùng
-        FB.api("/me", { fields: "name,email,picture" }, function (userInfo) {
-          alert("Chào " + userInfo.name + "! Bạn đã đăng nhập bằng Facebook.");
-          console.log("User Info:", userInfo);
+// function loginWithFacebook() {
+//   FB.login(
+//     function (response) {
+//       if (response.authResponse) {
+//         // Người dùng đã đăng nhập, lấy thông tin người dùng
+//         FB.api("/me", { fields: "name,email,picture" }, function (userInfo) {
+//           alert("Chào " + userInfo.name + "! Bạn đã đăng nhập bằng Facebook.");
+//           console.log("User Info:", userInfo);
 
-          // Hiển thị thông tin lên trang web (nếu cần)
-          document.getElementById("user-info").innerHTML = `
-                    <p><strong>Tên:</strong> ${userInfo.name}</p>
-                    <p><strong>Email:</strong> ${userInfo.email}</p>
-                    <img src="${userInfo.picture.data.url}" alt="Profile Picture">
-                `;
-        });
-      } else {
-        alert("Đăng nhập Facebook thất bại!");
-      }
-    },
-    { scope: "email,public_profile" }
-  );
-}
+//           // Hiển thị thông tin lên trang web (nếu cần)
+//           document.getElementById("user-info").innerHTML = `
+//                     <p><strong>Tên:</strong> ${userInfo.name}</p>
+//                     <p><strong>Email:</strong> ${userInfo.email}</p>
+//                     <img src="${userInfo.picture.data.url}" alt="Profile Picture">
+//                 `;
+//         });
+//       } else {
+//         alert("Đăng nhập Facebook thất bại!");
+//       }
+//     },
+//     { scope: "email,public_profile" }
+//   );
+// }
 // LogOut
-function logoutFromFacebook() {
-  FB.logout(function (response) {
-    alert("Bạn đã đăng xuất khỏi Facebook.");
-    document.getElementById("user-info").innerHTML = ""; // Xóa thông tin hiển thị
-  });
-}
+// function logoutFromFacebook() {
+//   FB.logout(function (response) {
+//     alert("Bạn đã đăng xuất khỏi Facebook.");
+//     document.getElementById("user-info").innerHTML = ""; 
+//   });
+// }
 
 
 // Show password
