f479b9ae65c6792779a5b00e19a0635bae46f75c
nguyenngoctruong31102003 <nguyenngoctruong31102003@gmail.com>
2025-04-01 00:58:28 +0700

    update

--- Script/script.js
+++ Script/script.js
@@ -1,10 +1,19 @@
 let menu = document.querySelector("#menu-icon");
 let menuist = document.querySelector(".menu-list");
 
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  menuist.classList.toggle("open");
-};
+document.addEventListener("DOMContentLoaded", function () {
+  let menu = document.querySelector("#menu-icon");
+  let menuist = document.querySelector(".menu-list");
+
+  if (menu && menuist) {
+    menu.onclick = () => {
+      menu.classList.toggle("bx-x");
+      menuist.classList.toggle("open");
+    };
+  } else {
+    console.error("Không tìm thấy phần tử menu hoặc menuist");
+  }
+});
 
 // preloader
 window.addEventListener("load", () => {
@@ -32,13 +41,19 @@ window.fbAsyncInit = function () {
 
   FB.AppEvents.logPageView();
 };
+
 (function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
-  js = d.createElement(s);
+  let js = d.createElement(s);
   js.id = id;
   js.src = "https://connect.facebook.net/en_US/sdk.js";
+  js.onload = function () {
+    console.log("Facebook SDK Loaded Successfully");
+  };
+  js.onerror = function () {
+    console.error("Facebook SDK Failed to Load");
+  };
+  let fjs = d.getElementsByTagName(s)[0];
   fjs.parentNode.insertBefore(js, fjs);
 })(document, "script", "facebook-jssdk");
 
